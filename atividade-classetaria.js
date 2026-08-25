d8bf240442a78ac22d864df0cd13a479e8266e6b
Isabelle <isabelle.ckanzler@gmail.com>
2026-08-25 20:28:04 -0300

    tr1

---
+++ atividade-classetaria.js
@@ -0,0 +1,18 @@
+function executarAtividade() {
+    alert( 
+        const idade= Number(prompt("Por favor, digite sua idade em anos:"))
+const meses= idade * 12
+const dias= idade *365
+alert("Você tem "+idade+" anos e "+meses+" meses e "+dias+" dias. ")
+
+
+
+
+
+
+    );
+}
+
+
+//Peça a idade de uma pessoa em anos e mostre a mesma idade em meses e em dias.    (Dica: Considere que 1 ano tem 12 meses e 365 dias)
+
