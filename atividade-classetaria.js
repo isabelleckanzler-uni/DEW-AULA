309c3274f2976b40250b67fd72d14698231c8af0
isabelle <isabelle.ckanzler@gmail.com>
2026-08-25 22:03:26 -0300

    tr2

--- atividade-classetaria.js
+++ atividade-classetaria.js
@@ -1,18 +1,19 @@
 function executarAtividade() {
-    alert( 
-        const idade= Number(prompt("Por favor, digite sua idade em anos:"))
-const meses= idade * 12
-const dias= idade *365
-alert("Você tem "+idade+" anos e "+meses+" meses e "+dias+" dias. ")
+    const idade = Number(prompt("Digite sua idade:"));
+    const evento = prompt("Digite o evento (Livre, 16 ou 18):").toLowerCase();
 
-
-
-
-
-
-    );
+    if (evento === "livre") {
+        alert("Resultado: Pode entrar\nMotivo: entrada livre.");
+    } else if (evento === "16" && idade >= 16) {
+        alert("Resultado: Pode entrar\nMotivo: idade mínima atingida.");
+    } else if (evento === "16" && idade < 16) {
+        alert("Resultado: Não pode entrar\nMotivo: idade inferior à classificação mínima de 16 anos.");
+    } else if (evento === "18" && idade >= 18) {
+        alert("Resultado: Pode entrar\nMotivo: idade mínima atingida.");
+    } else if (evento === "18" && idade < 18) {
+        alert("Resultado: Não pode entrar\nMotivo: idade inferior à classificação mínima de 18 anos.");
+    } else {
+        alert("Opção inválida! Digite Livre, 16 ou 18.");
+    }
 }
-
-
-//Peça a idade de uma pessoa em anos e mostre a mesma idade em meses e em dias.    (Dica: Considere que 1 ano tem 12 meses e 365 dias)
 
